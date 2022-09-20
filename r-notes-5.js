0d96a3d8946095753c9bd029dbbfccd97fd48530
Dominick Cortez <jdcortez2268@gmail.com>
2022-09-20 13:48:44 -0700

    Initial Commit.

---
+++ r-notes-5.js
@@ -0,0 +1,11 @@
+/***************************************************************************
+Course Title: React Front to back 2022
+Author: Traversy Media
+Description: Project 5, Desktop support app. Building to help practice MERN stack and Redux
+***************************************************************************/
+
+// MongoDB Compass: GUI that allows us to communicate and update MongoDB collections
+npm i express dotenv mongoose colors bcryptjs //bcryptjs is used to hash passwords, mongoose is ODM, express is framework for js environment
+
+npm i -D nodemon // allows server to hot reload
+"server": "nodemon backend/server.js" //add to scripts in package.json
