17bb66c9613879a6bdc5ad9f41a507402df3a56e
Dominick Cortez <jdcortez2268@gmail.com>
2022-09-20 14:44:00 -0700

    Add routes and controller.

--- r-notes-5.js
+++ r-notes-5.js
@@ -5,7 +5,9 @@ Description: Project 5, Desktop support app. Building to help practice MERN stac
 ***************************************************************************/
 
 // MongoDB Compass: GUI that allows us to communicate and update MongoDB collections
-npm i express dotenv mongoose colors bcryptjs //bcryptjs is used to hash passwords, mongoose is ODM, express is framework for js environment
+npm i express dotenv mongoose colors bcryptjs //bcryptjs is used to hash passwords, mongoose is ODM, express is framework for js environment, dotenv is for environment variables, colors is for editor styling?
 
 npm i -D nodemon // allows server to hot reload
 "server": "nodemon backend/server.js" //add to scripts in package.json
+
+// it is cleaner to have route that points to controller function that provides functionality rather than having all of the business logic inside the route
