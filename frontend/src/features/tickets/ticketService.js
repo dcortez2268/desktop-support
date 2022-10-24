86a0376c852768fc3e521754118dae9cc6bfdbea
Dominick Cortez <jdcortez2268@gmail.com>
2022-10-24 13:15:27 -0700

    Display single ticket and close ticket

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -27,9 +27,42 @@ const getTickets = async (token) => {
 
     return response.data
 }
+
+// Get user ticket
+const getTicket = async (ticketId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    }
+
+    const response = await axios.get(API_URL + ticketId, config)
+
+    return response.data
+}
+
+// close user ticket
+const closeTicket = async (ticketId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    }
+
+    const response = await axios.put(
+        API_URL + ticketId,
+        { status: 'closed' },
+        config
+    )
+
+    return response.data
+}
+
 const ticketService = {
     createTicket,
     getTickets,
+    getTicket,
+    closeTicket,
 }
 
 export default ticketService
